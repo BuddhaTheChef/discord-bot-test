41f54a35cca129836953c9bb1eca6f010b8173ed
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-03-05 11:22:41 -1000

    fixed file structure

---
+++ commands/report.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args) => {
+      let ourUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      if(!ourUser) return message.channel.send("Couldn't find user.");
+      let reason = args.join(" ").slice(22);
+
+      let reportEmbed = new Discord.RichEmbed()
+      .setDescription("Reports")
+      .setColor('#15f153')
+      .addField("Reported User", `${ourUser} with ID: ${ourUser.id}`)
+      .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+      .addField("Channel", message.channel)
+      .addField("Time", message.createdAt)
+      .addField("Reason", reason)
+
+      let reportschannel = message.guild.channels.find(`name`, "reports");
+      if(!reportschannel) return message.channel.send("Couldn't find reports channel.")
+
+      message.delete().catch(O_o => {});
+      reportschannel.send(reportEmbed);
+}
+
+module.exports.help = {
+  name: "report"
+}
