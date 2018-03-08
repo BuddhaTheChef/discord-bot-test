29f359ddd299ff592b8899ec51016e26167234e3
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-03-07 14:35:48 -1000

    tempmute finally works

--- commands/warn.js
+++ commands/warn.js
@@ -4,53 +4,55 @@ const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = (bot, message, args) => {
-if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No can do pal!");
-let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-if(!wUser) return message.reply("COuldn't find them yo!");
-if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("THEY ARE BETTER THAN YOU!");
-let reason = args.join(" ").slice(22);
 
-if(!warns[wUser.id]) warns[wUser.id] = {
-  warns: 0
-};
+  //!warn @daeshan <reason>
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No can do pal!");
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  if(!wUser) return message.reply("Couldn't find them yo");
+  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
+  let reason = args.join(" ").slice(22);
 
-warns[wUser.id].warns++;
+  if(!warns[wUser.id]) warns[wUser.id] = {
+    warns: 0
+  };
 
-fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
-  if(err) console.log(err);
-})
+  warns[wUser.id].warns++;
 
-let warnEmbed = new Discord.RichEmbed()
-.setDescription("Warns")
-.setAuthor(message.author.username)
-.setColor("#FC6400")
-.addField("Warned User", `<@${wUser.id}>`)
-.addField("Warned In", message.channel)
-.addField("Number of Warnings", warns[wUser.id].warns)
-.addField("Reason", reason);
+  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+    if (err) console.log(err)
+  });
 
-let warnchannel = message.guild.channels.find(`name`, "incidents");
-if(!warnchannel) return message.reply("Couldn't find the channel you were looking for??");
+  let warnEmbed = new Discord.RichEmbed()
+  .setDescription("Warns")
+  .setAuthor(message.author.username)
+  .setColor("#fc6400")
+  .addField("Warned User", `<@${wUser.id}>`)
+  .addField("Warned In", message.channel)
+  .addField("Number of Warnings", warns[wUser.id].warns)
+  .addField("Reason", reason);
 
-warnchannel.send(warnEmbed);
+  let warnchannel = message.guild.channels.find(`name`, "incidents");
+  if(!warnchannel) return message.reply("Couldn't find channel");
 
-if(warns[wUser.id].warns == 2) {
-  let muterole = message.guild.roles.find(`name`, "muted");
-  if(!muterole) return message.reply("You should create that role bruh.");
+  warnchannel.send(warnEmbed);
 
-  let mutetime = "10s";
-  (wUser.addRole(muterole.id));
-  message.channel.send(`<@${wUser.id}> has temporarily been muted`);
+  if(warns[wUser.id].warns == 2){
+    let muterole = message.guild.roles.find(`name`, "muted");
+    if(!muterole) return message.reply("You should create that role dude.");
 
-  setTimeout(function(){
-    wUser.removeRole(muterole.id)
-    mexsage.channel.reply(`<@${wUser.id}> have been unmuted.`)
-  }, ms(mutetime))
-}
-if(warns[wUser.id].warns == 3) {
-  message.guild.member(wUser).ban(reason);
-  message.reply(`<@${wUser.id}> has been banned.`)
-}
+    let mutetime = "10s";
+    await(wUser.addRole(muterole.id));
+    message.channel.send(`<@${wUser.id}> has been temporarily muted`);
+
+    setTimeout(function(){
+      wUser.removeRole(muterole.id)
+      message.reply(`<@${wUser.id}> has been unmuted.`)
+    }, ms(mutetime))
+  }
+  if(warns[wUser.id].warns == 3){
+    message.guild.member(wUser).ban(reason);
+    message.reply(`<@${wUser.id}> has been banned.`)
+  }
 
 }
 
