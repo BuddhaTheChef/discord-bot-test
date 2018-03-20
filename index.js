7807167047803b5f76684c8cb25c36916e21a8c1
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-03-19 14:18:11 -1000

    discordbot

--- index.js
+++ index.js
@@ -145,7 +145,30 @@ if(cmd === `${prefix}kick`) {
       rerturn;
 
   }
-
+//
+//   if(cmd === `${prefix}report`) {
+//
+//     let ourUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+//     if(!ourUser) return message.channel.send("Couldn't find user.");
+//     let reason = args.join(" ").slice(22);
+//
+//     let reportEmbed = new Discord.RichEmbed()
+//     .setDescription("Reports")
+//     .setColor('#15f153')
+//     .addField("Reported User", `${ourUser} with ID: ${ourUser.id}`)
+//     .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+//     .addField("Channel", message.channel)
+//     .addField("Time", message.createdAt)
+//     .addField("Reason", reason)
+//
+//     let reportschannel = message.guild.channels.find(`name`, "reports");
+//     if(!reportschannel) return message.channel.send("Couldn't find reports channel.")
+//
+//     message.delete().catch(O_o => {});
+//     reportschannel.send(reportEmbed);
+//     return;
+//   }
+//
   if(cmd === `${prefix}serverinfo`){
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
